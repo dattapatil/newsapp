c7cd90db05e346b32a9d11d41b3d2f3af50fa54c
Datta <dattatrayp52@gmail.com>
2022-09-12 00:01:19 +0530

    react router

--- src/App.js
+++ src/App.js
@@ -4,13 +4,32 @@ import React, { Component } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
 
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 export default class App extends Component {
   render() {
     return (
       <div>
-        <NavBar></NavBar>
-        <News country="us" category="sport" />
-      </div>
+      <BrowserRouter>     
+        <NavBar />        
+        <Routes>
+            <Route path="/" element={<News key="general" country="in" category="general" />}></Route>
+            <Route path="/business" element={<News key="business" country="in" category="business" />}></Route>
+            <Route path="/entertainment" element={<News key="entertainment" country="in" category="entertainment" />}></Route>
+            <Route path="/general" element={<News key="general" country="in" category="general" />}></Route>
+            <Route path="/health" element={<News key="health" country="in" category="health" />}></Route>
+            <Route path="/science" element={<News key="science" country="in" category="science" />}></Route>
+            <Route path="/sports" element={<News key="sports" country="in" category="sports" />}></Route>
+            <Route path="/technology" element={<News key="technology" country="in" category="technology" />}></Route>         
+        </Routes>        
+        </BrowserRouter>
+        </div>
+      
     )
   }
 }
